10345ee51f2d587d78b4e0dcb0c6e4294358b278
GustavoChico <josegustavochico@gmail.com>
2025-10-01 02:06:42 +0200

    web troubleshoot again

--- public/js/communications.js
+++ public/js/communications.js
@@ -209,14 +209,6 @@ export class Communications {
         },
         {
           urls: 'stun:global.stun.twilio.com:3478'
-        },
-        {
-          urls: [
-            "turn:openrelay.metered.ca:80",
-            "turn:openrelay.metered.ca:443"
-          ],
-          username: "openrelayproject",
-          credential: "openrelayproject"
         }
       ]
     };
